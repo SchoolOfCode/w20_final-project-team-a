af3ab272c37ec0ee2747bdf2871c893f1ac09705
Lewis Murray <lewisdmurray00@gmail.com>
2021-08-26 23:45:18 +0100

    Created the project showcase page and then currently creating a project component and styling it, will need to be replaced with the rendering later down the line

--- src/Pages/ProjectsShowcase.tsx
+++ src/Pages/ProjectsShowcase.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import '../Styling/ProjectsShowcase.css'
+import Projects from '../Components/FormInput/Projects'
 
 const Showcase = () => {
     return (
-        <div>
+        <div className="wrapper">
+
             
         </div>
     )
