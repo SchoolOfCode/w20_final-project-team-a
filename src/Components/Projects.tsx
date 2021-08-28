14c2e5eaa9bfc6476a2fbda25bbbac34e71e2b0b
Lewis Murray <lewisdmurray00@gmail.com>
2021-08-28 14:28:57 +0100

    Added the showcase title along the side

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -11,10 +11,14 @@ const Projects: React.FC<Props> = ({data}) => {
 
     return (
         <div className="wrapper">
+            
+            <header className="showcase-header">
+                <h1 className="showcase-header-text">Showcase</h1>
+            </header>
+
             <span className="project-name">{data.projectName}</span>        
             <img src={data.appDeploymentImage} alt="Project" className="main-image"/>
             <span className="project-date">12/05/2000</span>
-            
         </div>
     )
 }
