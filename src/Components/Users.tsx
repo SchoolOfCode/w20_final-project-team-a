a8478019fa0c274892b26b47f33ccd452f17e6aa
Becks Maybury <becksmaybury@hotmail.co.uk>
2021-08-31 17:14:13 +0100

    working on styling

--- src/Components/Users.tsx
+++ src/Components/Users.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import '../Styling/projects.css'
+import '../Styling/ProfilesShowcase.css'
 
 type Props = {
     data:any,
 }
 
 const Users: React.FC<Props> = ({data}) => {
-
+console.log(data)
     return (
-        <div className="wrapper">
+        <div >
             <span> {data.displayName}</span>
             <img src={data.photo} alt="bootcamper pic"/>
             <span> {data.cohort}</span>
             <span> {data.statement}</span>
+            <span> {data.email}</span>
             
         </div>
     )
--- src/Pages/ProfilesShowcase.tsx
+++ src/Pages/ProfilesShowcase.tsx
@@ -3,6 +3,9 @@ import React,{useState, useEffect} from 'react'
 import '../Styling/ProjectsShowcase.css'
 import Users from '../Components/Users'
 
+import sidebar from '../Components/BackgroundsPlus/ShowcaseUpLeft.png'
+import line from '../Components/BackgroundsPlus/Line.png'
+
 import axios from 'axios'
 import { API_URL } from '../config'
 
@@ -18,15 +21,18 @@ const ProfilesShowcase = () => {
             (response) => {
                 // response.data.forEach((user:any) => setData([...data,user]))
                 // console.log(response.data, "data = ",data)
-                let userDataArray = [];
-                for (let i=0; i<response.data.length; i++) {
-                    userDataArray.push(response.data[i]);
-                    console.log("response.data[i]", response.data[i])
-                    setData([...userDataArray, userDataArray])
-                    console.log(userDataArray);
-                }
-                }
-        )
+                // let userDataArray = [];
+                // for (let i=0; i<response.data.length; i++) {
+                //     userDataArray.push(response.data[i]);
+                //     console.log("response.data[i]", response.data[i])
+                //     setData([...userDataArray, userDataArray])
+                //     console.log(userDataArray);
+                // }
+                setData(response.data)
+                
+            }
+            )
+            console.log(data)
         }
     getData()
     }, [])
@@ -34,7 +40,18 @@ const ProfilesShowcase = () => {
     return (
         <>
             <div>
-                {data.map((user) => <Users data={user} />)}
+            <section className="profiles-showcase-page-container">
+                <header className="profiles-showcase-header">
+                    <h1 className="profiles-showcase-header-text">Login</h1>
+                    <img className="profiles-showcase-header-line" src={line} alt="sidebar"/>
+                </header>
+            </section>
+            <section className="profiles-showcase-display">
+                <  img src={sidebar} alt="dividing line" className="profiles-showcase-sidebar-image"/>
+            </section>
+            <section className="profiles-showcase-display-items">           
+                {data.map((user) => <Users data={user}  />)}
+            </section>
             </div>
         </>
     )
