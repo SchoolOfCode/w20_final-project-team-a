d08733bebde801674216a7a6578c4538f6dbb8ea
Lewis Murray <lewisdmurray00@gmail.com>
2021-09-03 23:35:42 +0100

    Added a onMouseEnter and leave event that does display the name when hovered over, will try create a mouseOver event next

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -12,28 +12,24 @@ const Projects: React.FC<Props> = ({data}) => {
 
     console.log(data.appDeploymentImage)
 
-    //const [isShown, setIsShown] = useState(false)
+    const [isShown, setIsShown] = useState(false)
     
     return (
     <>
         <div>
             <span className="project-showcase-title-text">{data.projectName}</span>        
-            
-            <div className="project-showcase-container-overlay">
-                <img
-                // onMouseEnter={()=>setIsShown(true)} 
-                //onMouseLeave={()=>setIsShown(false)}
-                src={data.appDeploymentImage}
-                alt="Project"
-                className="project-showcase-image"
-                />
-                <div className="project-showcase-overlay">
-                    <div className="project-showcase-overlay-text">
-                        {data.projectName}
-                    </div>
+            <img
+            onMouseEnter={()=>setIsShown(true)} 
+            onMouseLeave={()=>setIsShown(false)}
+            src={data.appDeploymentImage}
+            alt="Project"
+            className="project-showcase-image"
+            />
+            {isShown && (
+                <div>
+                    {data.projectName}
                 </div>
-            </div>
-            
+            )}
             <span className="project-showcase-date">12/05/2000</span>
         </div>
     </>
