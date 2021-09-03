47cc234bf174372239f6ad3e9862481f0ff07c6d
Lewis Murray <lewisdmurray00@gmail.com>
2021-09-03 21:38:40 +0100

    Starting to attempt the overlay

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../Styling/ProjectsShowcase.css'
 
 type Props = {
     data:any,
 }
 
+
+
+
 const Projects: React.FC<Props> = ({data}) => {
 
     console.log(data.appDeploymentImage)
 
+    //const [isShown, setIsShown] = useState(false)
+    
     return (
     <>
         <div>
             <span className="project-showcase-title-text">{data.projectName}</span>        
-            <img src={data.appDeploymentImage} alt="Project" className="project-showcase-image"/>
+            
+            <div className="project-showcase-container-overlay">
+                <img
+                // onMouseEnter={()=>setIsShown(true)} 
+                //onMouseLeave={()=>setIsShown(false)}
+                src={data.appDeploymentImage}
+                alt="Project"
+                className="project-showcase-image"
+                />
+                <div className="project-showcase-overlay">
+                    <div className="project-showcase-overlay-text">
+                        {data.projectName}
+                    </div>
+                </div>
+            </div>
+            
             <span className="project-showcase-date">12/05/2000</span>
         </div>
     </>
