b5026d6e17c6f6dc11b2e86bdf2b6b077907bad9
Lewis Murray <lewisdmurray00@gmail.com>
2021-09-11 11:23:45 +0100

    Waiting for homepage to be complete

--- src/Pages/01-Showcase/showcase-individual/ShowcaseProfile.tsx
+++ src/Pages/01-Showcase/showcase-individual/ShowcaseProfile.tsx
@@ -1,12 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import "./ShowcaseProfile.scss";
 import HorizontalCircuit from '../../../Components/ReactComponents/HorizontalCircuit/HorizontalCircuit';
-import LeftVerticalTitle from '../../../Components/ReactComponents/LeftVerticalTitle/LeftVerticalTitle';
 import { API_URL } from '../../../config';
 import axios from 'axios';
-import Loading from '../../../Components/ReactComponents/Loading/Loading';
 
-const ShowcaseProfile = (date:any) => {
+
+const ShowcaseProfile = (data: any) => {
     return (
         <div>
             <h1>mad</h1>
