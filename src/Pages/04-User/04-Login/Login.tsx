8e423d9f9a28265e104bcd7fab68f44945309bea
gschandan <gsc128@gmail.com>
2021-09-13 23:28:11 +0100

    .

--- src/Pages/04-User/04-Login/Login.tsx
+++ src/Pages/04-User/04-Login/Login.tsx
@@ -5,10 +5,10 @@ import { API_URL } from "../../../config";
 import "../04-User-Base.scss";
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import {UserSignupValidationSchema} from "../../../Components/ReactComponents/Signup/UserSignupValidationSchema"
+import {UserLoginValidationSchema} from "../../../Components/ReactComponents/Signup/UserLoginValidationSchema"
 // import pinhead from "../../../Components/VisualAssets/BackgroundsPlus/User Signup PinHead.png";
 import LeftVerticalTitle from "../../../Components/ReactComponents/LeftVerticalTitle/LeftVerticalTitle";
-
+import Loading from "../../../Components/ReactComponents/Loading/Loading";
 import CircleCircuit from "../../../Components/VisualAssets/BackgroundsPlus/CircleCircuit.svg"
 import CircuitHead from "../../../Components/VisualAssets/BackgroundsPlus/CircuitHead.svg"
 import CircuitYellow from "../../../Components/VisualAssets/BackgroundsPlus/CircuitHeadYellow.svg"
@@ -27,8 +27,13 @@ const Login: React.FC<Props> = ({ loginStatus, setLoginStatus }) => {
   const [success, setSuccess] = useState(false);
   const [failure, setFailure] = useState(false);
   const [failureMsg, setFailureMsg] = useState("");
-  const history = useHistory();
   const [submit, setSubmit] = useState(false);
+
+  const history = useHistory();
+
+  const {register, handleSubmit, formState: {errors}} = useForm<UserLoginForm>({
+    resolver: yupResolver(UserLoginValidationSchema)
+  })
 
   function handleLogin(e : React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     e.preventDefault();
@@ -71,51 +76,57 @@ const Login: React.FC<Props> = ({ loginStatus, setLoginStatus }) => {
 
   const [timer, setTimer] = useState(3);
 
-  useEffect(() => {
-    if (success === true && timer <= 0) {
-      setLoginStatus(true);
-      history.push("/dashboard");
-      return;
-    }
-    if (success) {
-      const loginRedirectTimer = setInterval(() => {
-        setTimer(timer - 1);
-      }, 1000);
-      return () => clearInterval(loginRedirectTimer);
-    }
-  }, [success, history, timer, setLoginStatus]);
+  // useEffect(() => {
+  //   if (success === true && timer <= 0) {
+  //     setLoginStatus(true);
+  //     history.push("/dashboard");
+  //     return;
+  //   }
+  //   if (success) {
+  //     const loginRedirectTimer = setInterval(() => {
+  //       setTimer(timer - 1);
+  //     }, 1000);
+  //     return () => clearInterval(loginRedirectTimer);
+  //   }
+  // }, [success, history, timer, setLoginStatus]);
 
   return (
     <div>
       <div className="user-page-container">
         <LeftVerticalTitle title="Login"></LeftVerticalTitle>
         <section className="user-form-container">
-          <section className="user-page-image">
-            <img src={CircuitHead} alt="head" className="user-page-image" />
+        <section className={`${success?"logging-in":"user-image-container"}`}>
+            <img src={CircleCircuit} alt="circle circuit design" className={`user-image-circuit-cricle`} />
+            {!success && !failure &&
+              <img src={CircuitYellow} alt="circuit head design" className="user-image-circuit-head" />
+            }
+            {!success && failure &&
+              <img src={CircuitHead} alt="circuit head design" className="user-image-circuit-incorrect" />
+            }
+            {success && !failure &&
+              <Loading className = "logging-in-spinner" loadingText="Logging in"/>
+            }              
           </section>
+        {!success &&
           <form className="user-form-input">
-            <section>
+            <section className="user-form-group">
               <label htmlFor="email">email address</label>
-              <p>
-                <input
-                  type="email"
-                  placeholder="lewis@lewis.ninja"
-                  name="email"
-                  id="email"
-                  // onChange={(e) => setEmail(e.target.value)}
-                ></input>
-              </p>
+              <input
+                type="email"
+                placeholder="lewis@lewis.ninja"
+                name="email"
+                id="email"
+                // onChange={(e) => setEmail(e.target.value)}
+              ></input>
             </section>
-            <section>
+            <section className="user-form-group">
               <label htmlFor="password">password</label>
-              <p>
-                <input
+              <input
                   type="password"
                   name="password"
                   id="password"
                   // onChange={(e) => setPassword(e.target.value)}
-                ></input>
-              </p>
+              ></input>
             </section>
             <section className="user-registered-link">
               Not registered? Click{" "}
@@ -124,19 +135,7 @@ const Login: React.FC<Props> = ({ loginStatus, setLoginStatus }) => {
               </a>{" "}
               to register
             </section>
-            <section className="user-submit">
-              <button
-                className="user-submit-button"
-                // disabled={!email || !password ? true : false}
-                type="submit"
-                onClick={(e) => handleLogin(e)}
-              >
-                Login
-              </button>
-            </section>
-          </form>
-
-          <section className="user-messages-container">
+            <section className="user-messages-container">
             {success && (
               <div className="user-messages-container-success">
                 <p className="user-messages-text-success">
@@ -150,6 +149,17 @@ const Login: React.FC<Props> = ({ loginStatus, setLoginStatus }) => {
               </div>
             )}
           </section>
+            <section className="user-submit">
+              <button
+                className="user-submit-button"
+                type="submit"
+                onClick={(e) => handleLogin(e)}
+              >
+                Login
+              </button>
+            </section>
+          </form>
+        }
         </section>
       </div>
     </div>
