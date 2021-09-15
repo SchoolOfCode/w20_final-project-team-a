439eff9eb8a636ef6329bb31476305286d204658
gschandan <gsc128@gmail.com>
2021-09-15 17:59:31 +0100

    added circuit to edit profile and project submit

--- src/Components/ReactComponents/EditProfile/PreviewImage.tsx
+++ src/Components/ReactComponents/EditProfile/PreviewImage.tsx
@@ -1,12 +1,16 @@
+import BGImage from "../../VisualAssets/BackgroundsPlus/Microchip.png"
 const PreviewImage = ({displayName,photo,cohort}:any) => {
     
 
     return (
+        <>
         <article className="edit-page-picture">
             <h2 className="edit-page-picture-greeting">{displayName}</h2>
             <img className = "edit-page-picture-image" src={photo} alt="The user" />
             <h3 className="edit-page-picture-cohort">Bootcamp {cohort}</h3>
         </article>
+        <img src={BGImage} className="edit-page-microchip" alt="microchip design item" />
+        </>
     )
 }
 
