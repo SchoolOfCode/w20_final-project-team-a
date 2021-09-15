69f71fb7c096d0e138e4011273f7d6752bfc90aa
Lewis Murray <lewisdmurray00@gmail.com>
2021-09-15 19:31:13 +0100

    Tyescript file designed/ borrowed

--- src/Pages/01-Showcase/showcase-individual/ShowcaseProfile.tsx
+++ src/Pages/01-Showcase/showcase-individual/ShowcaseProfile.tsx
@@ -3,12 +3,35 @@ import "./ShowcaseProfile.scss";
 import HorizontalCircuit from '../../../Components/ReactComponents/HorizontalCircuit/HorizontalCircuit';
 import { API_URL } from '../../../config';
 import axios from 'axios';
+import Loading from '../../../Components/ReactComponents/Loading/Loading';
+import circuitT from "../../Components/VisualAssets/BackgroundsPlus/HomeBGTop.png"
+import circuitB from "../../Components/VisualAssets/BackgroundsPlus/HomeBGBottom.png"
 
 
 const ShowcaseProfile = (data: any) => {
+
     return (
-        <div>
-            <h1>mad</h1>
+        <div className="wrapper">
+            <img className="circuitB" src={circuitB} alt="circuit-board" />
+            <img className="circuitT" src={circuitT} alt="circuit-board" />
+            
+            <p className="arrow-left">{"<"}</p>{" "}
+            <p>Image here.</p>
+            <p className="arrow-right">{">"}</p>{" "}
+
+            <HorizontalCircuit className="line-right"/>
+
+            <p className="description">Description Here</p>
+            <p className="tech">Tech here</p>
+            <p className="heading">Our Project</p>
+
+            <HorizontalCircuit className="line-left"/>
+
+            <p className="problem-statement">Problem Statement here</p>
+            <p className="contributors">Me, Me and Eemilio</p>
+            
+
+
         </div>
     )
 }
