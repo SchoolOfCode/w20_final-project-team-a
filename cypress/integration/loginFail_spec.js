0c8270ebbb8104e378449c335f7a48ea8b65cb09
Becks Maybury <becksmaybury@hotmail.co.uk>
2021-09-18 11:16:35 +0100

    login fail and success with redirect to dashboard added

---
+++ cypress/integration/loginFail_spec.js
@@ -0,0 +1,14 @@
+describe("Login Tests", () => {
+  it("tests if user is registered", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.get("#email")
+      .type("fake@email.com")
+      .should("have.value", "fake@email.com");
+    cy.get("#password").type("123456").should("have.value", "123456");
+    cy.get(".user-submit").click();
+    cy.get(".user-messages-text-failure").should(
+      "contain",
+      "That email is not registered"
+    );
+  });
+});
---
+++ cypress/integration/loginSuccess_spec.js
@@ -0,0 +1,11 @@
+describe("Login Tests", () => {
+  it("tests if user is registered", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.get("#email")
+      .type("user@fakemail.com")
+      .should("have.value", "user@fakemail.com");
+    cy.get("#password").type("123456").should("have.value", "123456");
+    cy.get(".user-submit").click();
+    cy.url().should("contain", "/dashboard");
+  });
+});
