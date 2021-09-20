afa567a41d1a86fc67c189a70f09975c0be6d04c
gschandan <gsc128@gmail.com>
2021-09-20 14:14:53 +0100

    .

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:5000/api/"
+export const API_URL = process.env.REACT_APP_ENV==="prod"?process.env.REACT_APP_PROD_HOST : process.env.REACT_APP_DEV_HOST
 export const VALID_IMAGE_TYPES = ['image/jpg', 'image/jpeg', 'image/gif', 'image/png']
 export const MAX_IMAGE_SIZE = 5 * 1024 * 1024
