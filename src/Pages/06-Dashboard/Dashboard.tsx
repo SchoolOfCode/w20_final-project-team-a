f3eb0bafdc8beb6efdf91b91938f120c5d14cf4c
gschandan <gsc128@gmail.com>
2021-09-20 17:03:29 +0100

    .

--- src/Pages/06-Dashboard/Dashboard.tsx
+++ src/Pages/06-Dashboard/Dashboard.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import { API_URL } from "../../config";
 import "./Dashboard.scss"
 import { useHistory } from "react-router-dom";
 import AdminBoard from "../../Components/ReactComponents/AdminBoard/AdminBoard";
@@ -11,7 +13,33 @@ type DashboardProps = {
 };
 
 const Dashboard: React.FC<DashboardProps> = ({ loginStatus, currentUser, setLoginStatus}) => {
+  
+  const [currentUserData, setCurrentUserData] = useState<any>({});
 
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const authCheck = await axios.get(API_URL + "auth/check", {
+          withCredentials: true,
+        });
+        const isLoggedIn = await authCheck.data.success;
+        if (await isLoggedIn) {
+          setLoginStatus(true);
+          setCurrentUserData(authCheck.data.user);
+        } else {
+          setLoginStatus(false);
+        }
+      } catch (err) {
+        let errorMessage =
+          "An error occurred. Please refresh the page and try again.";
+        if (err instanceof Error) {
+          errorMessage = err.message;
+        }
+        console.log(errorMessage);
+      }
+    };
+    checkAuth();
+  }, [setLoginStatus]);
 
   const history = useHistory();
   if (!currentUser || loginStatus === false) history.push("/login");
@@ -21,7 +49,7 @@ const Dashboard: React.FC<DashboardProps> = ({ loginStatus, currentUser, setLogi
       {currentUser.role === "admin" && <AdminBoard />}
       {currentUser.displayName && currentUser.role !== "admin" && 
         <EditProfile 
-          user={currentUser}
+          user={currentUserData}
           setLoginStatus={ setLoginStatus}
         />}
     </>
